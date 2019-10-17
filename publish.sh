3a981b3dba39e17de3579f68bf50a1acbb1f0591
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-10-17 22:49:28 +0200

    start directly again

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 rm -rf /home/tfenster/github/end2end/*
 cp -r /mnt/c/Users/tfenster8982/GitHub/end2end/* /home/tfenster/github/end2end/
 cd /home/tfenster/github/end2end/
-echo './publish.sh && rake publish'
+rake
